7f22ec07f8c59dc6fb9e7b74f08ae6e1275e08b7
Chikawali <chykoblanko@gmail.com>
2024-01-23 07:09:56 -0800

    all done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
+"""A  function that queries the Reddit API and returns the number of
+   subscribers (not active users, total subscribers) for a given subreddit.
+   If an invalid subreddit is given, the function should return 0.
 """
-number of subscribers for a given subreddit
-"""
-
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns number of subscribers for a given subreddit or 0 if subreddit
+       is invalid.
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+        r = requests.get(url, headers={'User-agent': 'hello-student 0.1'})
+        subs = r.json()
+        return subs.get('data').get('subscribers')
+    except AttributeError:
         return 0
